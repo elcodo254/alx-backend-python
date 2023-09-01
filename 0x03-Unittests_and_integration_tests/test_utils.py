e5b3233b9ce6b24fbce7d0ee2aa132665ca8026c
elcodo254 <ellawachaga@gmail.com>
2023-09-01 12:26:37 +0000

    fix bug + task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@ Test SUITE Unittest module Task
 """
 
 import unittest
-from parametized import parametized
+from parameterized import parameterized
 
 from utils import access_nested_map
 
@@ -13,7 +13,7 @@ class TestAccessNestedMap(unittest.TestCase):
     """
     class for testing Nested Map function
     """
-    @parametized.expand([
+    @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
@@ -24,3 +24,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         real_output = access_nested_map(map, path)
         self.assertEqual(real_output, expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, map, path, wrong_output):
+        """
+        Test method raises correct exception
+        """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(map, path)
+            self.assertEqual(wrong_output, e.exception)
