a68b51f58ddda513b1903d7a795e389a167b989d
elcodo254 <ellawachaga@gmail.com>
2023-09-18 00:35:12 +0000

    to string from float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function than takes in a float and returns a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns a string
+    Args: n - float
+    """
+    return str(n)
