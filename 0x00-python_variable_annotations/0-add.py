84e23440eba8938c1ae90df1237f5ccb55df464d
elcodo254 <ellawachaga@gmail.com>
2023-09-18 00:15:39 +0000

    basic annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+type-annoted function add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns sum of a and b
+    args: a - float
+          b - float
+    """
+    return a + b
