c52d7f294c0cc2b609023f77f27e6b3abebf4cbe
elcodo254 <ellawachaga@gmail.com>
2023-09-18 00:30:13 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type-annoted function that returns floor of float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns larn=gest int value less or equal to n
+    args: n - float
+    """
+    return int(n) if n >=0 else int(n) - 1
