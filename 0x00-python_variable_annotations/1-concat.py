d3ccfd01a2ccd1356e3aa6f5c22ed023d8d52200
elcodo254 <ellawachaga@gmail.com>
2023-09-18 00:22:13 +0000

    task1: concatenating

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+type-annoted function that concatenates 2 strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns a concatenated string
+    args: str1 - string
+          str2 - string
+    """
+    return str1 + str2
